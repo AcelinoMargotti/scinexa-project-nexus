0381a87a06831ad61cd5e808e4eb834bf1e269a8
Acelino Margotti <165937963+acelinomargotti-v2@users.noreply.github.com>
2025-06-09 10:01:04 -0300

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
@@ -13,6 +13,7 @@ import ProjectDetails from "./pages/ProjectDetails";
 import Calendar from "./pages/Calendar";
 import CreateProject from "./pages/CreateProject";
 import NotFound from "./pages/NotFound";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
@@ -22,7 +23,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <AuthProvider>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
@@ -33,7 +34,7 @@ const App = () => (
             <Route path="/calendar" element={<Calendar />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
